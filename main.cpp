f463169d125bd0cdf187393396643ea63fac5f30
Steve <coding812@gmail.com>
2025-04-09 19:20:34 -0400

    Added functionality to return if system call fails

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+// Helper function to execute a command and check for errors
+void runCommand(const string& command) {
+    if (system(command.c_str()) != 0) {
+        cerr << "Failed to execute: " << command << endl;
+    }
+}
+
 int main (int argc, char* argv[] ) {
 	
 	if ( argc < 2 ){
@@ -12,15 +19,15 @@ int main (int argc, char* argv[] ) {
 	string theme = argv[1];
 	
 	if ( theme == "dark" ){
-		system("dconf write /org/cinnamon/theme/name \"'Mojave-Dark'\"");
-		system("dconf write /org/cinnamon/theme/gtk-theme \"'Mojave-Dark'\""); // works for desktop theme
-		system("dconf write /org/cinnamon/desktop/interface/gtk-theme \"'Mojave-Dark'\"");
+		(runCommand)("dconf write /org/cinnamon/theme/name \"'Mojave-Dark'\"");
+		(runCommand)("dconf write /org/cinnamon/theme/gtk-theme \"'Mojave-Dark'\""); // works for desktop theme
+		(runCommand)("dconf write /org/cinnamon/desktop/interface/gtk-theme \"'Mojave-Dark'\"");
 		cout << "Theme Set to dark" << endl;
 	}
 	else if ( theme == "light" ){
-		system("dconf write /org/cinnamon/theme/name \"'Mojave-Light'\"");
-		system("dconf write /org/cinnamon/theme/gtk-theme \"'Mojave-Light'\""); // works for desktop theme
-		system("dconf write /org/cinnamon/desktop/interface/gtk-theme \"'Mojave-Light'\"");
+		(runCommand)("dconf write /org/cinnamon/theme/name \"'Mojave-Light'\"");
+		(runCommand)("dconf write /org/cinnamon/theme/gtk-theme \"'Mojave-Light'\""); // works for desktop theme
+		(runCommand)("dconf write /org/cinnamon/desktop/interface/gtk-theme \"'Mojave-Light'\"");
 		cout << "Theme set to light" << endl;
 	}
 	else {
