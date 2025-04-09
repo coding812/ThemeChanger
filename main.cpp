24b6a259838b2d2b1efd28eb5b26ae7def3d70c4
Steve <coding812@gmail.com>
2025-04-09 13:44:15 -0400

    Successfully switches between two pre-determined themes

--- main.cpp
+++ main.cpp
@@ -3,18 +3,23 @@
 using namespace std;
 
 int main (int argc, char* argv[] ) {
+	string theme = argv[1];
 
 	if ( argc < 2 ){
 		cout << "Please provide an input for the theme" << endl;
 		return 1;
 	}
 
-	string theme = argv[1];
-
 	if ( theme == "dark" ){
+		system("dconf write /org/cinnamon/theme/name \"'Mojave-Dark'\"");
+		system("dconf write /org/cinnamon/theme/gtk-theme \"'Mojave-Dark'\""); // works for desktop theme
+		system("dconf write /org/cinnamon/desktop/interface/gtk-theme \"'Mojave-Dark'\"");
 		cout << "Theme Set to dark" << endl;
 	}
 	else if ( theme == "light" ){
+		system("dconf write /org/cinnamon/theme/name \"'Mojave-Light'\"");
+		system("dconf write /org/cinnamon/theme/gtk-theme \"'Mojave-Light'\""); // works for desktop theme
+		system("dconf write /org/cinnamon/desktop/interface/gtk-theme \"'Mojave-Light'\"");
 		cout << "Theme set to light" << endl;
 	}
 	else {
